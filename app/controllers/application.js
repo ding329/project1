d8f9849288c225a13b22bfb527749076ad5f8921
ding329 <bwilwerding@gmail.com>
2015-09-04 17:29:33 -0700

    completed step 15, added download of pictures from flicker

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,17 @@ import Ember from 'ember';
 var Photo = Ember.Object.extend({
 	title: '',
 	username: '',
-	url: '',
+//flicker data
+	owner: '',
+	id: '', 
+	farm: 0,
+	secret: '',
+	server: '',
+	url: function(){
+		return "https://farm"+this.get('farm')+
+		".staticflickr.com/"+this.get('server')+
+		"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
+	}.property('farm','server','id','secret'),
 });
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -43,21 +53,55 @@ testPhotos.pushObject(testimg2);
 testPhotos.pushObject(testimg3);
 testPhotos.pushObject(testimg4);
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(
+{
 	photos: testPhotos,
 	searchField: '',
-	filteredPhotos: function() {
+	filteredPhotos: function()
+	{
 		var filter = this.get('searchField');
 		var rx = new RegExp(filter, 'gi');
 		var photos = this.get('photos');
 
-		return photos.filter(function(photo){
+		return photos.filter(function(photo)
+		{
 			return photo.get('title').match(rx) || photo.get('username').match(rx);
 		});
-	}.property('photos', 'searchField'),
-	actions: {
-		search: function () {
+	}.property('photos.@each', 'searchField'),
+	actions:
+	{
+		search: function ()
+		{
 			this.get('filteredPhotos');
-		}
+		},
+		getPhotos: function()
+		{
+			var apiKey = 'ca97d9ed8cedadb2e203d17214645451';
+			var host = 'https://api.flickr.com/services/rest/';
+			var method = "flickr.tags.getClusterPhotos";
+			var tag = "hi";
+			var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
+			var photos = this.get('photos');
+			Ember.$.getJSON(requestURL, function(data)
+			{//callback for successfully completed requests
+				console.log(data);
+				data.photos.photo.map(function(photo) 
+				{
+					var newPhotoItem = Photo.create(
+					{
+						title: photo.title,
+						username: photo.username,
+						//flickr extra data
+						owner: photo.owner,
+						//flickr url data
+						id: photo.id,
+						farm: photo.farm,
+						secret: photo.secret,
+						server: photo.server,
+					});
+					photos.pushObject(newPhotoItem);
+				});  //added semi colom
+			});
+		},
 	}
 });
